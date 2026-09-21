5c43bb9c604ecde419f3188df6a66f71df2c4086
Артур <arthur089808@gmail.com>
2026-09-22 01:15:18 +0700

    Connect DemoScreen

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -34,6 +34,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
